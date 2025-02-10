1938ca7226697e85a6ce0ccc343a470a3c0fe541
0xdeadbad <matheus.dasilva.garcias@gmail.com>
2025-02-10 09:31:06 -0300

    match configuration.nix and home.nix versions

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -137,7 +137,6 @@
     shell = pkgs.zsh;
   };
 
-
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
   home-manager.backupFileExtension = "backup";
@@ -186,43 +185,43 @@
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-    # List services that you want to enable:
+  # List services that you want to enable:
 
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.11"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
 
-    nix.settings.trusted-users = [
-      "root"
-      "garicas"
-    ];
+  nix.settings.trusted-users = [
+    "root"
+    "garicas"
+  ];
 
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-    # Will use eventually
-    programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = [
-      # Add any missing dynamic libraries for unpackaged programs
-      # here, NOT in environment.systemPackages
-    ];
+  # Will use eventually
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
 }
